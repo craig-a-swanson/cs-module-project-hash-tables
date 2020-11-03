b9d5b620f4e5d66754ff353a9739f5947e6f13cc
Craig Swanson <cswanson02@me.com>
2020-11-03 10:09:08 -0600

    Implemented lookup_table.py challenge.

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -10,12 +10,32 @@ def slowfun_too_slow(x, y):
 
     return v
 
+
+power_cache = {}
+factorial_cache = {}
+
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    # Your code here
+
+    # start populating a cache for the power function
+    if (x, y) not in power_cache:
+        power_cache[(x, y)] = math.pow(x, y)
+    # retrieve the value associated with the cache key
+    v = power_cache[(x, y)]
+
+    # start populating a cache for the factorial function
+    if v not in factorial_cache:
+        factorial_cache[v] = math.factorial(v)
+    # retrieve the value associated with the cache key
+    v = factorial_cache[v]
+    
+    v //= (x + y)
+    v %= 982451653
+
+    return v
 
 
 
