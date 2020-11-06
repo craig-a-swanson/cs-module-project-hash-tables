78261d4ff20f9f3557d9579a768c4fd2a961174d
Craig Swanson <cswanson02@me.com>
2020-11-06 13:33:54 -0600

    Added logic for all subtraction permutations.

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -19,15 +19,23 @@ def sumdiff(set):
         function_results[element] = result
 
     addition_results = {}
+    subtraction_results = {}
     set_length = len(set)
     for index in range(set_length):
         cur_number = set[index]
-        for next_index in range(index + 1, set_length):
-            result = function_results[cur_number] + function_results[set[next_index]]
-            addition_results[result] = (cur_number, set[next_index])
+        for next_index in range(0, set_length):
+            if set[next_index] == cur_number:
+                continue
+            addition_result = function_results[cur_number] + function_results[set[next_index]]
+            if addition_result not in addition_results:
+                addition_results[addition_result] = (cur_number, set[next_index])
+            subtraction_result = function_results[cur_number] - function_results[set[next_index]]
+            subtraction_results[subtraction_result] = (cur_number, set[next_index])
 
 
     for key, value in addition_results.items():
+        print(f'{key}: {value}')
+    for key, value in subtraction_results.items():
         print(f'{key}: {value}')
 
 sumdiff(q)
