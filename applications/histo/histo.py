dc9507b6d9102c1c3494a504b5d81e30c9a4bb03
Craig Swanson <cswanson02@me.com>
2020-11-05 22:00:30 -0600

    In histo.py, added hash sign counts and foratted. Still need secondary alpha sorting.

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -8,6 +8,7 @@ def word_count(file_name):
 
     word_dict = {}
     parsed_words = words.split()
+    longest_word = 0
 
     for word in parsed_words:
         lower_word = word.lower()
@@ -16,14 +17,21 @@ def word_count(file_name):
             word_dict[cleaned_word] = 0
         word_dict[cleaned_word] += 1
 
+        # check word length for clean print out
+        if len(cleaned_word) > longest_word:
+            longest_word = len(cleaned_word)
+
     # source of following statement: https://stackoverflow.com/questions/613183/how-do-i-sort-a-dictionary-by-value
     word_dict = {key: value for key, value in sorted(word_dict.items(), key=lambda item: item[1], reverse=True)}
+
+    for word, count in word_dict.items():
+        hash_string = '#'*count
+        space_string = ' '*(longest_word + 2 - len(word))
+        print(f'{word}:{space_string}{hash_string}')
     
     return word_dict
 
-robin_test = word_count("robin.txt")
-for key in robin_test.items():
-    print(key)
+word_count('robin.txt')
 
 # can use negatives to sort when reverse() can't be used
 # can sort() on more than one item at a time
