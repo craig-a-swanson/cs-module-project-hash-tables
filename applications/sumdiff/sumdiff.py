5f60cbe2b1d158e3473bd978d881a25cf3fc9fd2
Craig Swanson <cswanson02@me.com>
2020-11-06 13:23:12 -0600

    Added logic to build dictionary of all addition combinations

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -11,5 +11,23 @@ q = (1, 3, 4, 7, 12)
 def f(x):
     return x * 4 + 6
 
-# Your code here
+function_results = {}
 
+def sumdiff(set):
+    for element in set:
+        result = f(element)
+        function_results[element] = result
+
+    addition_results = {}
+    set_length = len(set)
+    for index in range(set_length):
+        cur_number = set[index]
+        for next_index in range(index + 1, set_length):
+            result = function_results[cur_number] + function_results[set[next_index]]
+            addition_results[result] = (cur_number, set[next_index])
+
+
+    for key, value in addition_results.items():
+        print(f'{key}: {value}')
+
+sumdiff(q)
